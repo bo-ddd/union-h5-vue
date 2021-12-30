0dfbb6de451f8b65fde4a04e5b28099a9729da4b
“chineseii” <3237806134@qq.com>
2021-12-30 14:34:51 +0800

    modify

--- src/views/RankList/Selected.vue
+++ src/views/RankList/Selected.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
-      Selected(精选)
+    <div class="Rotation">
+      <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
+        <van-swipe-item>1</van-swipe-item>
+        <van-swipe-item>2</van-swipe-item>
+        <van-swipe-item>3</van-swipe-item>
+        <van-swipe-item>4</van-swipe-item>
+      </van-swipe>
+      <div class="trend">
+        <div class="left"></div>
+        <div class="right"></div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,6 +21,28 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+  .Rotation{
+    width: 355px;
+    height: 168px;
+    margin: 0  auto;
+    & .my-swipe .van-swipe-item {
+      color: #fff;
+      font-size: 20px;
+      line-height: 168px;
+      text-align: center;
+      background-color: #39a9ed;
+    }
+    & .trend{
+      width: 355px;
+      height: 60px;
+      margin: 20px auto;
+      border: 1px solid black;
+      display: flex;
+      & .left{
+        width: 85px;
+        height: 60px;
+      }
+    }
+  }
 </style>
