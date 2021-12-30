2ac53364b388788d562223c6d620cca453eb8a1c
qq931807848 <931807848@qq.com>
2021-12-30 14:32:25 +0800

    排行榜

--- src/views/RankList/RankHome.vue
+++ src/views/RankList/RankHome.vue
@@ -1,22 +1,28 @@
 <template>
   <div class="wrap">
     <div class="header">
-      <van-icon @click="toHomePage" class="icon-left" name="arrow-left" />
-      <div class="follow">
-        <van-icon name="like-o" />
-        <span>关注</span>
+      <div class="left">
+        <van-icon @click="toHomePage" class="icon-left" name="arrow-left" />
+        <div class="follow">
+          <van-icon name="like-o" />
+          <span>关注</span>
+        </div>
       </div>
-      <van-tabs v-model="defauluActive" animated>
-        <van-tab
-          class="tab"
-          v-for="(title, index) in list"
-          :title="title.meta.title"
-          :key="index"
-          :to="title.path"
-        >
-        </van-tab>
-      </van-tabs>
-      <van-icon class="icon-right" name="ellipsis" />
+      <div class="center">
+        <van-tabs v-model="defauluActive" animated>
+          <van-tab
+            class="tab"
+            v-for="(title, index) in list"
+            :title="title.meta.title"
+            :key="index"
+            :to="title.path"
+          >
+          </van-tab>
+        </van-tabs>
+      </div>
+      <div class="right">
+          <van-icon class="icon-right" name="ellipsis" />
+      </div>
     </div>
     <div class="main">
       <router-view></router-view>
@@ -54,23 +60,19 @@ export default {
 
 <style lang="less" scoped>
 .wrap {
-  &.header {
+  & .header {
     padding: 14px;
     display: flex;
-    justify-content: center;
-    align-content: center;
+    & .left{
+        display: flex;
+        justify-content: center;
+        align-content: center;
     & .icon-left {
       margin-top: 10px;
       margin-left: 20px;
       font-size: 30px;
     }
-    & .icon-right {
-      margin-top: 10px;
-      //   margin-right: 15px;
-      font-size: 30px;
-      margin-left: 84px;
-    }
-    & .follow {
+      & .follow {
       margin-top: 14px;
       margin-right: 20px;
       padding: 0px 10px;
@@ -80,9 +82,21 @@ export default {
       color: white;
       border-radius: 20px;
     }
+    }
+    & .right{
+            & .icon-right {
+      margin-top: 10px;
+      //   margin-right: 15px;
+      font-size: 30px;
+      margin-left: 94px;
+    }
+    }
+  
   }
 }
 ::v-deep .van-tab {
+    font-size: 20px;
   line-height: 25px;
+  color: #a8a2a2;
 }
 </style>
